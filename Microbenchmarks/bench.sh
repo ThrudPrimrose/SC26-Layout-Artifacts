56fa640f31911fa2512897208e10911ac49afaf7
Yakup Koray Budanaz <ybudanaz@ethz.ch>
2026-03-20 11:48:47 +0100

    Some cleanup

--- Microbenchmarks/bench.sh
+++
@@ -1,7 +0,0 @@
-#Compile with desired sizes
-nvcc -arch=sm_90a -O3 -Xcompiler=-fopenmp  \
-    -std=c++17 \
-    -o segmented_reduction segmented_reduction.cu
-
-# run a mode (results append to CSV)
-./segmented_reduction
