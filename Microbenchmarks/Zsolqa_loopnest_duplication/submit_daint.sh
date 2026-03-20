64f036ef60342b988dab9a8a6099a4e6fba11869
Yakup Koray Budanaz <ybudanaz@ethz.ch>
2026-03-20 13:35:36 +0100

    Add submission scripts

---
+++ Microbenchmarks/Zsolqa_loopnest_duplication/submit_daint.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+#SBATCH --job-name=zsolqa
+#SBATCH --nodes=1
+#SBATCH --partition=debug
+#SBATCH --exclusive
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=288
+#SBATCH --time=00:29:59
+#SBATCH --output=zsolqa_output_daint.txt
+#SBATCH --error=zsolqa_error_daint.txt
+
+export _DACE_NO_SYNC=1
+export OMP_NUM_THREADS=288
+export OMP_PROC_BIND=true
+export OMP_PLACES=cores
+export OMP_DISPLAY_ENV=TRUE
+
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
+pyenv activate dace_py_12
+
+export _DACE_NO_SYNC=1
+export LD_LIBRARY_PATH=$(spack location -i cuda@12.9)/lib64:$LD_LIBRARY_PATH
+spack load cuda@12.9
+spack load gcc/76jw6nu
+
+sleep 1
+python zsolqa_loopnest.py --variant=3d-gpu --reps=100 --csv bench_3d_gpu.csv
+sleep 1
+python zsolqa_loopnest.py --variant=split-gpu --reps=100 --csv bench_split_gpu.csv
+sleep 1
+python zsolqa_loopnest.py --variant=3d-reduce-gpu --reps=100 --csv bench_reduce_gpu.csv
+sleep 1
+python zsolqa_loopnest.py --variant=split-reduce-gpu --reps=100 --csv bench_reduce_split_gpu.csv
+sleep 1
+python zsolqa_loopnest.py --variant=3d --reps=100 --csv bench_3d.csv
+sleep 1
+python zsolqa_loopnest.py --variant=split --reps=100 --csv bench_split.csv
+sleep 1
+python zsolqa_loopnest.py --variant=3d-reduce --reps=100 --csv bench_reduce.csv
+sleep 1
+python zsolqa_loopnest.py --variant=split-reduce --reps=100 --csv bench_reduce_split.csv
+
+mv *.csv results/daint
+
+#sleep 1
+#ncu --set full --import-source=yes -f -o profile_3d python zsolqa_loopnest.py --variant=3d-gpu --reps=2 --csv __tr.csv
+#sleep 1
+#ncu --set full --import-source=yes -f -o profile_split python zsolqa_loopnest.py --variant=split-gpu --reps=2 --csv __tr.csv
+#sleep 1
+#ncu --set full --import-source=yes -f -o profile_reduce python zsolqa_loopnest.py --variant=reduce-gpu --reps=2 --csv __tr.csv
+#sleep 1
+#ncu --set full --import-source=yes -f -o profile_reduce python zsolqa_loopnest.py --variant=reduce_split-gpu --reps=2 --csv __tr.csv
