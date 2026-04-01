6d16272b3867d41d91dd3df314038470ac043d11
Yakup Koray Budanaz <ybudanaz@ethz.ch>
2026-04-01 15:04:47 +0200

    add scritps

---
+++ Microbenchmarks/Zekin_Loopnest_v3/submit_daint.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#SBATCH --job-name=zekin_d
+#SBATCH --nodes=1
+#SBATCH --partition=normal
+#SBATCH --time=01:30:00
+#SBATCH --output=zekin_d_%j.out
+#SBATCH --error=zekin_d_%j.err
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=288
+
+# -------------------------------
+# OpenMP configuration
+# -------------------------------
+export OMP_NUM_THREADS=288
+export OMP_PROC_BIND=spread
+export OMP_PLACES=cores
+
+# Optional: better NUMA behavior
+export OMP_DISPLAY_ENV=TRUE
+
+
+echo "Running on $(hostname)"
+echo "Threads: $OMP_NUM_THREADS"
+
+spack load gcc/76jw6nu # 14.3
+spack load cutensor
+spack load cuda@12.9
+
+# -------------------------------
+# Workload parameters (BIG!)
+# -------------------------------
+set -e
+export CUTENSOR_HOME=$(spack location -i cutensor)
+source ${SCRATCH}/yakup-dev-env/bin/activate
+export CUDA_HOME=$(spack location -i cuda@12.9)
+export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:$LD_LIBRARY_PATH
+
+export C_INCLUDE_PATH=$SCRATCH/include:$CUTENSOR_HOME/include:$C_INCLUDE_PATH
+export CPLUS_INCLUDE_PATH=$SCRATCH/include:$CUTENSOR_HOME/include:$CPLUS_INCLUDE_PATH
+export LIBRARY_PATH=$SCRATCH/lib:$SCRATCH/lib64:$CUTENSOR_HOME/lib/12:$CUDA_HOME/lib64:$LIBRARY_PATH
+export LD_LIBRARY_PATH=$SCRATCH/lib:$SCRATCH/lib64:$CUTENSOR_HOME/lib/12:$CUDA_HOME/lib64:$LD_LIBRARY_PATH
+export PATH=$SCRATCH/bin:$CUTENSOR_HOME/bin:$PATH
+
+
+nvcc -O3 -std=c++17 \
+    -arch=sm_90 \
+    -Xcompiler=-fopenmp -ffast-math \
+    -Xcompiler="-march=native -ffast-math -mtune=native" \
+    --use_fast_math \
+    -o bench_gpu_d bench_gpu.cu
+
+
+
+./bench_gpu_d
+./bench_cpu_d
---
+++ Microbenchmarks/Zekin_Loopnest_v3/submit_daint_cpu.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#SBATCH --job-name=zekin_d_c
+#SBATCH --nodes=1
+#SBATCH --partition=normal
+#SBATCH --time=00:30:00
+#SBATCH --output=zekin_d_%j.out
+#SBATCH --error=zekin_d_%j.err
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=288
+
+# -------------------------------
+# OpenMP configuration
+# -------------------------------
+export OMP_NUM_THREADS=288
+export OMP_PROC_BIND=spread
+export OMP_PLACES=cores
+
+# Optional: better NUMA behavior
+export OMP_DISPLAY_ENV=TRUE
+
+
+echo "Running on $(hostname)"
+echo "Threads: $OMP_NUM_THREADS"
+
+spack load gcc/76jw6nu # 14.3
+spack load cutensor
+spack load cuda@12.9
+
+# -------------------------------
+# Workload parameters (BIG!)
+# -------------------------------
+set -e
+export CUTENSOR_HOME=$(spack location -i cutensor)
+source ${SCRATCH}/yakup-dev-env/bin/activate
+export CUDA_HOME=$(spack location -i cuda@12.9)
+export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:$LD_LIBRARY_PATH
+
+export C_INCLUDE_PATH=$SCRATCH/include:$CUTENSOR_HOME/include:$C_INCLUDE_PATH
+export CPLUS_INCLUDE_PATH=$SCRATCH/include:$CUTENSOR_HOME/include:$CPLUS_INCLUDE_PATH
+export LIBRARY_PATH=$SCRATCH/lib:$SCRATCH/lib64:$CUTENSOR_HOME/lib/12:$CUDA_HOME/lib64:$LIBRARY_PATH
+export LD_LIBRARY_PATH=$SCRATCH/lib:$SCRATCH/lib64:$CUTENSOR_HOME/lib/12:$CUDA_HOME/lib64:$LD_LIBRARY_PATH
+export PATH=$SCRATCH/bin:$CUTENSOR_HOME/bin:$PATH
+
+g++ -O3 -std=c++17 \
+    -march=native \
+    -fopenmp \
+    -ffast-math \
+    -mtune=native \
+    -fno-vect-cost-model \
+    -ftree-vectorize \
+    -o bench_cpu_d bench_cpu.cpp
+
+./bench_cpu_d
