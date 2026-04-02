7b34dbf42903e8e45575966534e3696be0377958
Yakup Koray Budanaz <ybudanaz@ethz.ch>
2026-04-02 12:03:16 +0200

    Clean trash

--- Microbenchmarks/Transpose/p.py
+++
@@ -1,3 +0,0 @@
-print("hipcc  -O3 -ffast-math -fPIC -Wall -Wextra  -DHIP_PLATFORM_AMD=1 -D__HIP_PLATFORM_AMD__=1  "
-             "-Wno-unused-parameter -munsafe-fp-atomics --offload-arch=gfx942 "
-             "-ffp-contract=fast -Wno-ignored-attributes -Wno-unused-result -std=c++17 transpose_hiptensor.cpp -o transpose_lib -lhiptensor")
