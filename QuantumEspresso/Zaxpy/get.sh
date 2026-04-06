8f2b57a6237d87d6c25966c99e5aec2c08ba254d
Yakup Koray Budanaz <ybudanaz@ethz.ch>
2026-04-06 15:33:25 +0200

    Get kernel

---
+++ QuantumEspresso/Zaxpy/get.sh
@@ -0,0 +1,3 @@
+wget https://polybox.ethz.ch/index.php/s/ySAEwfRTSH4Jzmr/download/bin_indirect_accesses-20260406T132129Z-1-001.zip
+unzip bin_indirect_accesses-20260406T132129Z-1-001.zip
+mv bin_indirect_access bin
